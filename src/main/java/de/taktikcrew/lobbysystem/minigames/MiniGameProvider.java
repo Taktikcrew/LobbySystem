d01bba8da7b14ce7015cad4e5b92bfda31201fe6
Smoofy19 <nxcoop19@gmail.com>
2025-03-22 00:07:41 +0100

    Added ConnectFourManager

--- src/main/java/de/taktikcrew/lobbysystem/minigames/MiniGameProvider.java
+++ src/main/java/de/taktikcrew/lobbysystem/minigames/MiniGameProvider.java
@@ -1,6 +1,7 @@
 package de.taktikcrew.lobbysystem.minigames;
 
 import de.taktikcrew.lobbysystem.Lobby;
+import de.taktikcrew.lobbysystem.minigames.connection.connectfour.ConnectFourManager;
 import de.taktikcrew.lobbysystem.minigames.connection.tictactoe.TicTacToeManager;
 import lombok.Getter;
 import lombok.experimental.Accessors;
@@ -11,11 +12,13 @@ public class MiniGameProvider {
 
     private final Lobby lobby;
 
+    private final ConnectFourManager connectFourManager;
     private final TicTacToeManager ticTacToeManager;
 
     public MiniGameProvider(Lobby lobby) {
         this.lobby = lobby;
 
+        this.connectFourManager = new ConnectFourManager(this.lobby);
         this.ticTacToeManager = new TicTacToeManager(this.lobby);
     }
 }
