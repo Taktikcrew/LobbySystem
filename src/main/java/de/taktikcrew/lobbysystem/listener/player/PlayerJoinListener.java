071b0ab5849545d34abb812cfddbc10ef41b12cb
Smoofy19 <nxcoop19@gmail.com>
2025-03-23 20:13:26 +0100

    Set player join defaults

--- src/main/java/de/taktikcrew/lobbysystem/listener/player/PlayerJoinListener.java
+++ src/main/java/de/taktikcrew/lobbysystem/listener/player/PlayerJoinListener.java
@@ -4,9 +4,12 @@ import de.smoofy.core.api.Core;
 import de.taktikcrew.lobbysystem.Lobby;
 import de.taktikcrew.lobbysystem.database.LobbyPlayerDAO;
 import de.taktikcrew.lobbysystem.objects.LobbyPlayer;
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class PlayerJoinListener implements Listener {
 
@@ -23,13 +26,32 @@ public class PlayerJoinListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         var player = event.getPlayer();
+        var corePlayer = Core.instance().corePlayerProvider().corePlayer(player);
 
         this.lobbyPlayerDAO.create(new LobbyPlayer(player.getUniqueId(), false));
         var optionalLobbyPlayer = this.lobbyPlayerDAO.get(player.getUniqueId());
-        optionalLobbyPlayer.ifPresent(lobbyPlayer -> {
+
+        if (optionalLobbyPlayer.isPresent()) {
+            var lobbyPlayer = optionalLobbyPlayer.get();
+
             if (!lobbyPlayer.dsgvoAccepted()) {
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SLOWNESS, Integer.MAX_VALUE, Integer.MAX_VALUE, false, false, false));
+                player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, Integer.MAX_VALUE, Integer.MAX_VALUE, false, false, false));
+
                 Core.instance().coreTask().later(() -> player.openInventory(this.lobby.inventoryProvider().dsgvoInventory().inventory()), 1);
+
+                return;
             }
-        });
+        }
+        player.clearActivePotionEffects();
+        player.setCollidable(false);
+        player.setGameMode(GameMode.SURVIVAL);
+        if (player.hasPermission("lobby.fly")) {
+            player.setAllowFlight(true);
+        }
+        player.setLevel(0);
+        player.setExp(0);
+
+        this.lobby.inventoryProvider().lobbyPlayerInventory().setLobbyInventory(corePlayer);
     }
 }
